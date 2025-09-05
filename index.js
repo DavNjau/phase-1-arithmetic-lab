1d38c11315853e3062120475d389856422631321
David <nnjui13@gmail.com>
2025-09-05 13:25:31 +0300

    updated arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 2
+const num2 = 31
+const num3 = 10
+const num4 = 6
+const set = ["6", "8", "14", "17", "20"]
 
-// Write your code here
+const multiply = num1*num2
+const random = Math.floor(Math.random() * 100) + 1;
+const mod = num3 % num4
+const max = Math.max(...set)
